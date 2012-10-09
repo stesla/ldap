96a97eb183993f138373912a100102bf47d3c8ce
Samuel Tesla <samuel.tesla@gmail.com>
2012-10-09 10:33:05 -0500

    rearrange code

--- asn1/decode.go
+++ asn1/decode.go
@@ -6,71 +6,6 @@ import (
 	"reflect"
 )
 
-func decodeType(r io.Reader, buf []byte) (class, tag int, isCompound bool, err error) {
-	_, err = r.Read(buf[0:1])
-	if err != nil {
-		return
-	}
-
-	class = int(buf[0] >> 6)
-	isCompound = buf[0]&0x20 == 0x20
-
-	if c := buf[0] & 0x1f; c < 0x1f {
-		tag = int(c)
-	} else {
-		_, err = r.Read(buf[0:1])
-		if err != nil {
-			return
-		}
-
-		if buf[0]&0x7f == 0 {
-			err = SyntaxError{"long-form tag"}
-			return
-		}
-
-		for {
-			tag = tag<<7 | int(buf[0]&0x1f)
-
-			if buf[0]&0x80 == 0 {
-				break
-			}
-
-			_, err = r.Read(buf[0:1])
-			if err != nil {
-				return
-			}
-		}
-	}
-	return
-}
-
-func decodeLength(r io.Reader, buf []byte) (length int, isIndefinite bool, err error) {
-	_, err = r.Read(buf[0:1])
-	if err != nil {
-		return
-	}
-
-	if c := buf[0]; c < 0x80 {
-		length = int(c)
-	} else if c == 0x80 {
-		isIndefinite = true
-	} else if c == 0xff {
-		err = SyntaxError{"long-form length"}
-		return
-	} else {
-		var width int
-		n := c & 0x7f
-		width, err = io.ReadFull(r, buf[0:n])
-		if err != nil {
-			return
-		}
-		for _, b := range buf[0:width] {
-			length = length<<8 | int(b)
-		}
-	}
-	return
-}
-
 type Decoder struct {
 	r io.Reader
 }
@@ -144,6 +79,71 @@ func (dec *Decoder) decodeRawValue() (out RawValue, err error) {
 		_, err = io.ReadFull(dec.r, out.Bytes)
 		if err != nil {
 			return
+		}
+	}
+	return
+}
+
+func decodeType(r io.Reader, buf []byte) (class, tag int, isCompound bool, err error) {
+	_, err = r.Read(buf[0:1])
+	if err != nil {
+		return
+	}
+
+	class = int(buf[0] >> 6)
+	isCompound = buf[0]&0x20 == 0x20
+
+	if c := buf[0] & 0x1f; c < 0x1f {
+		tag = int(c)
+	} else {
+		_, err = r.Read(buf[0:1])
+		if err != nil {
+			return
+		}
+
+		if buf[0]&0x7f == 0 {
+			err = SyntaxError{"long-form tag"}
+			return
+		}
+
+		for {
+			tag = tag<<7 | int(buf[0]&0x1f)
+
+			if buf[0]&0x80 == 0 {
+				break
+			}
+
+			_, err = r.Read(buf[0:1])
+			if err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
+func decodeLength(r io.Reader, buf []byte) (length int, isIndefinite bool, err error) {
+	_, err = r.Read(buf[0:1])
+	if err != nil {
+		return
+	}
+
+	if c := buf[0]; c < 0x80 {
+		length = int(c)
+	} else if c == 0x80 {
+		isIndefinite = true
+	} else if c == 0xff {
+		err = SyntaxError{"long-form length"}
+		return
+	} else {
+		var width int
+		n := c & 0x7f
+		width, err = io.ReadFull(r, buf[0:n])
+		if err != nil {
+			return
+		}
+		for _, b := range buf[0:width] {
+			length = length<<8 | int(b)
 		}
 	}
 	return
