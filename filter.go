d36adb827f2d120d45dd6f352ea52279fe4c7c72
Samuel Tesla <samuel.tesla@gmail.com>
2013-01-08 15:33:55 -0600

    added Matches filter

--- filter.go
+++ filter.go
@@ -7,22 +7,35 @@ import (
 type Filter interface {}
 
 func And(filters ...Filter) Filter {
-	return asn1.OptionValue{"tag:0", filters}
+	return asn1.OptionValue{"tag:0,set", filters}
 }
 
 func Or(filters ...Filter) Filter {
-	return nil
+	return asn1.OptionValue{"tag:1,set", filters}
 }
 
 func Not(filter Filter) Filter {
-	return nil
+	return asn1.OptionValue{"tag:2", filter}
 }
 
 type attributeValueAssertion struct {
-	description, value []byte
+	Attribute, Value []byte
 }
 
-func Equals(description, value string) Filter {
-	val := attributeValueAssertion{[]byte(description),	[]byte(value)}
+func Equals(attribute, value string) Filter {
+	val := attributeValueAssertion{[]byte(attribute),	[]byte(value)}
 	return asn1.OptionValue{"tag:3", val}
+}
+
+type matchingRuleAssertion struct {
+	MatchingRule []byte `asn1:"tag:1,optional"`
+	Type []byte `asn1:"tag:2,optional"`
+	MatchValue []byte `asn1:"tag:3"`
+	DnAttributes bool `asn1:"tag:4"`
+}
+
+func Matches(rule, attribute, value string) Filter {
+	val := matchingRuleAssertion{
+		[]byte(rule), []byte(attribute), []byte(value), false}
+	return asn1.OptionValue{"tag:9", val}
 }
